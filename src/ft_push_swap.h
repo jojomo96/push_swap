4e56c731d6e16b9d901f1a5d893e447870767dba
Johannes Moritz <jmoritz@3-A-4.42heilbronn.de>
2024-04-11 14:49:07 +0200

    add functions to header

--- src/ft_push_swap.h
+++ src/ft_push_swap.h
@@ -6,18 +6,21 @@
 /*   By: jmoritz < jmoritz@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 00:55:15 by jmoritz           #+#    #+#             */
-/*   Updated: 2024/04/11 13:49:04 by jmoritz          ###   ########.fr       */
+/*   Updated: 2024/04/11 14:43:36 by jmoritz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PUSH_SWAP_H
-#define FT_PUSH_SWAP_H
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <limits.h>
-#include <unistd.h>
+# define FT_PUSH_SWAP_H
 
 # include "../lib/lib.h"
+# include <limits.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+t_dlist	*ft_global_stack_a(void);
+t_dlist	*ft_global_stack_b(void);
+void	ft_global_stacks_destroy(void);
 
 #endif
